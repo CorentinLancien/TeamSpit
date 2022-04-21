ad99fb646212d0039c5a07f6b63a6e72d70c3989
unknown <c.lancien@iia-laval.fr>
2022-04-21 12:27:02 +0200

    add architecture + services

---
+++ TeamSpit/Services/IUtilisateurService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamSpit.Model;
+
+namespace TeamSpit.Services
+{
+    public interface IUtilisateurService
+    {
+        public Utilisateur findById();
+    }
+}
